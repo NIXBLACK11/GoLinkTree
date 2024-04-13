bf5f8a74d5da67dc092680b9789ba74ecd8e5fdb
NIXBLACK11 <Siddharthsinghrana11@gmail.com>
2024-04-13 12:23:58 +0530

    seperate folder for frontend and backend for easy deployment

--- frontend/src/pages/User.tsx
+++
@@ -1,9 +0,0 @@
-import { Links } from "../components/Links";
-
-export const User = () => {
-    return (
-        <div className="dark:bg-gray-900 h-screen">
-            <Links/>
-        </div>
-    );
-}
